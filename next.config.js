625d0015ecf62e54fa553425bc07cdb89a3e991c
zhongqihua <zhongqihua@xmiles.cn>
2023-12-14 16:42:40 +0800

    test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    publicRuntimeConfig: {
+        API: process.env.TEST_API
+    }
 }
 
 module.exports = nextConfig
---
+++ src/pages/page/test.tsx
@@ -0,0 +1,11 @@
+import getConfig from "next/config"
+
+const { publicRuntimeConfig } = getConfig()
+
+const api = publicRuntimeConfig.API
+
+function Test() {
+    return <div style={{padding: '10px', margin: '0 auto'}}>
+        {api}
+    </div>
+}
